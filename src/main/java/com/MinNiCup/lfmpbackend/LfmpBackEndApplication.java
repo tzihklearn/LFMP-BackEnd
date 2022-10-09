63a3630b6701afedc265ee583d46fe26f69cfdf4
DoudiNCer <doudi@outlook.lv>
2022-10-09 15:41:14 +0800

    Chore: 基本环境配置、POM依赖、CommonResult、日志系统、MyBatis

--- src/main/java/com/MinNiCup/lfmpbackend/LfmpBackEndApplication.java
+++ src/main/java/com/MinNiCup/lfmpbackend/LfmpBackEndApplication.java
@@ -1,9 +1,17 @@
 package com.MinNiCup.lfmpbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@MapperScan("com.MinNiCup.lfmpbackend.mapper")
 @SpringBootApplication
+@CrossOrigin
+@EnableTransactionManagement
+@EnableScheduling
 public class LfmpBackEndApplication {
 
     public static void main(String[] args) {
