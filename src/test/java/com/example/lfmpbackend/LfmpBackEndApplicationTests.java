2eaef31313b034ebca4f640bcfdaf151abe133be
tzihklearn <1922941549@qq.com>
2022-10-09 14:27:59 +0800

    Initial commit

---
+++ src/test/java/com/example/lfmpbackend/LfmpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lfmpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfmpBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
